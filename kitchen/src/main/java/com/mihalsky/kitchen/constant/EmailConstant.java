4528ff0d3f631eaa52c69a3f560ff0da12827b21
Роман Михальский <roman.mi4alsky@gmail.com>
2021-07-06 15:14:00 +0300

    add finished project

--- kitchen/src/main/java/com/mihalsky/kitchen/constant/EmailConstant.java
+++ kitchen/src/main/java/com/mihalsky/kitchen/constant/EmailConstant.java
@@ -2,8 +2,8 @@ package com.mihalsky.kitchen.constant;
 
 public class EmailConstant {
   public static final String SIMPLE_MAIL_TRANSFER_PROTOCOL = "smtps";
-  public static final String USERNAME = "r.mi4alsky@gmail.com";
-  public static final String PASSWORD = "gmail12345G";
+  public static final String USERNAME = "********";//email
+  public static final String PASSWORD = "*******";//email password
   public static final String FROM_EMAIL = "support@luv2code.com";
   public static final String CC_EMAIL = "";
   public static final String EMAIL_SUBJECT = "Luv2code, LLC - Новый пароль";
